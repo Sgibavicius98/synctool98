c2e10541ff6d00fa61917e7711b5933e673fcc2a
Sgibavicius98 <65221600+Sgibavicius98@users.noreply.github.com>
2020-05-12 11:55:08 +0300

    Add files via upload

---
+++ SaltoSyncTool.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saltosynctool;
+
+/**
+ *
+ * @author 58088
+ */
+public class SaltoSyncTool {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+//        InterfaceLangas SINK = new InterfaceLangas();
+//        SINK.setVisible(true);
+        
+        Login LOGIN = new Login();
+        LOGIN.setVisible(true);
+        
+    }
+    
+}
